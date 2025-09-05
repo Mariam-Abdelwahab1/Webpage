4840ad3c7a2a195814ad0989c509ab44a85aec99
unknown <mariamabdelwahab30@gmail.com>
2025-09-05 15:18:38 +0300

    creating new folder for js course tasks.

---
+++ js/my_first_js_task.js
@@ -0,0 +1,6 @@
+// There are 3 different ways to declare a variable in JavaScript: var, let, and const. I'm gonna use 'let' here.
+let studentName = "Ali";
+let studentAge = 20;
+let studentGrade = 12;
+
+console.log("The student " + studentName + " is " + studentAge + " years old and is in grade " + studentGrade + ".");
